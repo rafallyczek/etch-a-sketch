b47b8017ae706bb3a78f431b49e8a1e0dadf9237
rafallyczek <rafal.lyczek@gmail.com>
2023-02-09 11:26:40 +0100

    Add hover effect to squareDivs
    Added event listener which calls background color changing function
    when user hovers over suqareDiv.

--- javascript/script.js
+++ javascript/script.js
@@ -2,5 +2,11 @@ const container  = document.querySelector(".container");
 
 for(let i=0;i<(16*16);i++){
     const squareDiv = document.createElement("div");
+    squareDiv.classList.add("squareDiv");
     container.appendChild(squareDiv);
 }
+
+const squareDivs = document.querySelectorAll(".squareDiv");
+squareDivs.forEach(squareDiv => squareDiv.addEventListener("mouseover",function(){
+    this.style.backgroundColor = "blue";
+}));
