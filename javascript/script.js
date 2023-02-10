da898085ec86c140e6a583b75e65a2bf356c5e1d
rafallyczek <rafal.lyczek@gmail.com>
2023-02-10 12:00:05 +0100

    Add option to change grid size
    Added function which changes grid size (number of squares) and modified
    existing code to work with changes.

--- javascript/script.js
+++ javascript/script.js
@@ -1,32 +1,67 @@
 const container  = document.querySelector(".container");
 const resetButton = document.querySelector("#reset");
+const setGridButton = document.querySelector("#setGrid");
+let squareDivs;
 let mouseDown = false;
+let gridSize = 16;
+let size = 400 / gridSize;
 
-for(let i=0;i<(16*16);i++){
-    const squareDiv = document.createElement("div");
-    squareDiv.classList.add("squareDiv");
-    container.appendChild(squareDiv);
+drawGrid();
+addEventListeners();
+
+function drawGrid(){
+
+    for(let i=0;i<gridSize**2;i++){
+        const squareDiv = document.createElement("div");
+        squareDiv.setAttribute("style",`width: ${size}px; height: ${size}px; background-color: white`);
+        squareDiv.classList.add("squareDiv");
+        container.appendChild(squareDiv);
+    }
+
 }
 
-const squareDivs = document.querySelectorAll(".squareDiv");
+function addEventListeners(){
 
-squareDivs.forEach(squareDiv => {
-    squareDiv.addEventListener("mousedown",function(){
-        mouseDown = true;
-        this.style.backgroundColor = "black";
-    });
-    squareDiv.addEventListener("mouseup",function(){
-        mouseDown = false;
-    });
-    squareDiv.addEventListener("mouseover",function(){
-        if(mouseDown){
+    squareDivs = document.querySelectorAll(".squareDiv");
+
+    squareDivs.forEach(squareDiv => {
+        squareDiv.addEventListener("mousedown",function(){
+            mouseDown = true;
             this.style.backgroundColor = "black";
-        }
+        });
+        squareDiv.addEventListener("mouseup",function(){
+            mouseDown = false;
+        });
+        squareDiv.addEventListener("mouseover",function(){
+            if(mouseDown){
+                this.style.backgroundColor = "black";
+            }
+        });
     });
-});
+
+}
 
 resetButton.addEventListener("click",reset);
 
 function reset(){
     squareDivs.forEach(squareDiv => squareDiv.style.backgroundColor = "white");
+}
+
+setGridButton.addEventListener("click",setGrid);
+
+function setGrid(){
+
+    gridSize = prompt("enter number between 10 and 100:");
+    if(!gridSize || isNaN(gridSize)){
+        return;
+    }
+    gridSize = +gridSize;
+    if(gridSize<10 || gridSize>100){
+        return;
+    }
+    size = 400 / gridSize;
+    container.replaceChildren();
+    drawGrid();
+    addEventListeners();
+
 }
